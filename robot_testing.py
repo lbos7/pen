fb69581c51992919750cd0652f2096192b7888a1
Logan Boswell <loganstuartboswell@gmail.com>
2024-09-20 11:27:44 -0500

    Created Robot classs and modified robot_testing to accomodate changes

--- robot_testing.py
+++ robot_testing.py
@@ -1,35 +1,41 @@
 from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
-from interbotix_common_modules.common_robot.robot import robot_shutdown, robot_startup
+from robot import Robot
 from math import pi
 # The robot object is what you use to control the robot
-robot = InterbotixManipulatorXS("px100", "arm", "gripper")
+interbotix_robot = InterbotixManipulatorXS("px100", "arm", "gripper")
+robot = Robot(interbotix_robot)
 
-robot_startup()
+robot.start_running()
 mode = 'h'
-print(robot.arm.group_info.joint_names)
 # Let the user select the position
 while mode != 'q':
-    mode=input("[h]ome, [s]leep, [go] - open gripper, [gc] - close gripper, [wccw] - waist counter-clockwise, [wcw] - waist clockwise, [shu] - shoulder up,\n[shd] - shoulder down, [eu] - elbow up, [ed] - elbow down, [q]uit ")
+    mode=input("[h]ome, [s]leep, [go] - open gripper, [gc] - close gripper, [wccw] - waist counter-clockwise, [wcw] - waist clockwise, [shu] - shoulder up,\n[shd] - shoulder down, [eu] - elbow up, [ed] - elbow down, [pos] - display position, [r,theta,z] - move to position in cylindrical coordinates [q]uit ")
     if mode == "h":
-        robot.arm.go_to_home_pose()
+        robot.home_pos()
     elif mode == "s":
-        robot.arm.go_to_sleep_pose()
+        robot.sleep_pos()
     elif mode == "gc":
-        robot.gripper.grasp()
+        robot.grab()
     elif mode == "go":
-        robot.gripper.release()
+        robot.release()
     elif mode == "wccw":
-        robot.arm.set_single_joint_position('waist', pi/6)
+        robot.robot.arm.set_single_joint_position('waist', pi/6)
     elif mode == "wcw":
-        robot.arm.set_single_joint_position('waist', -pi/6)
+        robot.robot.arm.set_single_joint_position('waist', -pi/6)
     elif mode == "shd":
-        robot.arm.set_single_joint_position('shoulder', pi/6)
+        robot.robot.arm.set_single_joint_position('shoulder', pi/6)
     elif mode == "shu":
-        robot.arm.set_single_joint_position('shoulder', -pi/6)
+        robot.robot.arm.set_single_joint_position('shoulder', -pi/6)
     elif mode == "ed":
-        robot.arm.set_single_joint_position('elbow', pi/6)
+        robot.robot.arm.set_single_joint_position('elbow', pi/6)
     elif mode == "eu":
-        robot.arm.set_single_joint_position('elbow', -pi/6)
+        robot.robot.arm.set_single_joint_position('elbow', -pi/6)
+    elif mode == "pos":
+        print(robot.get_ee_pos_cart())
+        print(robot.get_ee_pos_cyl())
+    elif "," in mode:
+        cords = mode.split(",")
+        robot.move_ee_pos_cyl_abs(float(cords[0]), float(cords[1]), float(cords[2]))
 
 
-robot_shutdown()
+robot.stop_running()
