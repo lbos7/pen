be2ce569b3a74569eecbe7e2cd57addcf101cc13
Logan Boswell <loganstuartboswell@gmail.com>
2024-09-20 12:57:52 -0500

    Removed trackbars in camera_practice

--- camera_practice.py
+++ camera_practice.py
@@ -7,14 +7,6 @@ def f(x):
 
 cam = Camera()
 
-cv2.namedWindow('Main', cv2.WINDOW_NORMAL)
-cv2.createTrackbar('H High','Main',179,179, lambda x:x)
-cv2.createTrackbar('H Low','Main',0,179, lambda x:x)
-cv2.createTrackbar('S High','Main',255,255, lambda x:x)
-cv2.createTrackbar('S Low','Main',0,255, lambda x:x)
-cv2.createTrackbar('V High','Main',255,255, lambda x:x)
-cv2.createTrackbar('V Low','Main',0,255, lambda x:x)
-
 while True:
     frames = cam.get_frames()
     color_image = cam.get_color_image(frames)
